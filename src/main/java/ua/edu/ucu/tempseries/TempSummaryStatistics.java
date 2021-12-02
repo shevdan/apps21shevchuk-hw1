f892918674acabbbbd1c90fc21f5afe93fd75d44
Bohdan Shevchuk <shevdan007@gmail.com>
2021-12-02 15:01:58 +0200

    Update TempSummaryStatistics.java

--- src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/edu/ucu/tempseries/TempSummaryStatistics.java
@@ -1,7 +1,6 @@
 package ua.edu.ucu.tempseries;
 
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public class TempSummaryStatistics {
@@ -10,7 +9,7 @@ public class TempSummaryStatistics {
     private double minTemp;
     private double maxTemp;
 
-    public TempSummaryStatistics(double avg, double dev, double min, double max){
+    public TempSummaryStatistics(double avg,double dev,double min,double max) {
         this.avgTemp = avg;
         this.devTemp = dev;
         this.minTemp = min;
